35c56f58b6f042c29713d5399bdc0c3337636307
jgittler <jason.gittler@gmail.com>
2015-06-07 23:47:45 -0700

    add sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ Rails.application.routes.draw do
   #   end
   root to: redirect("/kickstarter")
   get "kickstarter", to: "landing#kickstarter", as: "kickstarter"
+
+  get "sitemap.xml", to: "sitemap#show", format: "xml", as: "sitemap"
 end
