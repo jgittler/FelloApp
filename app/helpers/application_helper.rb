37cc4344f030efeea10b6f86f87ca7a4bc0a4b37
jgittler <jason.gittler@gmail.com>
2015-08-28 02:26:25 -0700

    moving towards new layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,25 @@
 module ApplicationHelper
   def konvoy_one
-    cl_image_tag 'blueprint_konvoy.jpg', alt: 'Konvoy Designs For Fello Sunglasses'
+    cl_image_tag 'blueprint_konvoy_2.jpg', alt: 'Konvoy Designs For Fello Sunglasses'
   end
 
   def konvoy_two
-    cl_image_tag 'fello_eyewear_with_ranger.jpg', alt: 'Fello Eyewear At The Golden Gate Bridge'
-  end
-
-  def konvoy_three
-    cl_image_tag 'blueprint_konvoy.jpg', alt: 'Konvoy Designs For Fello Sunglasses'
+    cl_image_tag 'jerome_konvoy.jpg', alt: 'Fello Eyewear At The Golden Gate Bridge'
   end
 
   def fantom_one
-    cl_image_tag 'blueprint_fantom.jpg', alt: 'Fantom Designs For Fello Sunglasses'
+    cl_image_tag 'blueprint_fantom_hd.jpg', alt: 'Fantom Designs For Fello Sunglasses'
   end
 
   def fantom_two
-    cl_image_tag 'coming_soon.jpg', alt: 'Fantom Line Coming Soon'
-  end
-
-  def fantom_three
-    cl_image_tag 'blueprint_fantom.jpg', alt: 'Fantom Designs For Fello Sunglasses'
+    cl_image_tag 'coming_soon_2.jpg', alt: 'Fantom Line Coming Soon'
   end
 
   def heron_one
-    cl_image_tag 'blueprint_heron.jpg', alt: 'Heron Line Coming Soon'
+    cl_image_tag 'blueprint_heron_hd.jpg', alt: 'Heron Line Coming Soon'
   end
 
   def heron_two
-    cl_image_tag 'coming_soon.jpg', alt: 'Heron (Aviator) Designs For Fello Sunglasses'
-  end
-
-  def heron_three
-    cl_image_tag 'blueprint_heron.jpg', alt: 'Heron Line Coming Soon'
+    cl_image_tag 'coming_soon_2.jpg', alt: 'Heron (Aviator) Designs For Fello Sunglasses'
   end
 end
