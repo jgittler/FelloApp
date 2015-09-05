0eedfda845afd12498e08afc9bc587c231765ee5
jgittler <jason.gittler@gmail.com>
2015-09-04 17:24:49 -0700

    preping for deploy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,40 +11,4 @@ module ApplicationHelper
       image_tag name, options
     end
   end
-
-  def bb_center
-    image_tag 'bbfello.jpg'
-  end
-
-  def bb_top_left
-    image_tag 'ridge_down.jpg'
-  end
-
-  def bb_left
-    image_tag 'cody.jpg'
-  end
-
-  def bb_bottom_left
-    image_tag 'matt.jpg'
-  end
-
-  def bb_top
-    image_tag 'kayrah.jpg'
-  end
-
-  def bb_bottom
-    image_tag 'cate.jpg'
-  end
-
-  def bb_top_right
-    image_tag 'jerome.jpg'
-  end
-
-  def bb_right
-    image_tag 'michelle.jpg'
-  end
-
-  def bb_bottom_right
-    image_tag 'kassan.jpg'
-  end
 end
--- app/models/jumbo_image.rb
+++ app/models/jumbo_image.rb
@@ -7,17 +7,17 @@ class JumboImage
     end
   end
 
-  def self.pier
-    if Rails.env.production?
-    else
-      'top_level.jpg'
-    end
-  end
-
   def self.vosh
     if Rails.env.production?
     else
       'fello_and_vosh.jpg'
+    end
+  end
+
+  def self.cate_lg
+    if Rails.env.production?
+    else
+      'cate_lg.jpg'
     end
   end
 
@@ -60,6 +60,118 @@ class JumboImage
     if Rails.env.production?
     else
       'eye_cover.jpg'
+    end
+  end
+
+  def self.tl_1
+    if Rails.env.production?
+    else
+      'matt.jpg'
+    end
+  end
+
+  def self.tm_1
+    if Rails.env.production?
+    else
+      'owen.jpg'
+    end
+  end
+
+  def self.tr_1
+    if Rails.env.production?
+    else
+      'kayrah.jpg'
+    end
+  end
+
+  def self.ml_1
+    if Rails.env.production?
+    else
+      'ridge_down.jpg'
+    end
+  end
+
+  def self.mm_1
+    if Rails.env.production?
+    else
+      'fello_center.png'
+    end
+  end
+
+  def self.mr_1
+    if Rails.env.production?
+    else
+      'dani.jpg'
+    end
+  end
+
+  def self.bl_1
+    if Rails.env.production?
+    else
+      'cate.jpg'
+    end
+  end
+
+  def self.bm_1
+    if Rails.env.production?
+    else
+      'kassan.jpg'
+    end
+  end
+
+  def self.br_1
+    if Rails.env.production?
+    else
+      'jerome_smile.jpg'
+    end
+  end
+
+  def self.tl_xs_1
+    if Rails.env.production?
+    else
+      'matt-xs.jpg'
+    end
+  end
+
+  def self.tr_xs_1
+    if Rails.env.production?
+    else
+      'kayrah-xs.jpg'
+    end
+  end
+
+  def self.ml_xs_1
+    if Rails.env.production?
+    else
+      'ridge_down-xs.jpg'
+    end
+  end
+
+  def self.mm_xs_1
+    if Rails.env.production?
+    else
+      'fello_center-xs.png'
+    end
+  end
+
+  def self.mr_xs_1
+    if Rails.env.production?
+    else
+      'dani-xs.jpg'
+    end
+  end
+
+  def self.bl_xs_1
+    if Rails.env.production?
+    else
+      'cate-xs.jpg'
+    end
+  end
+
+  def self.bm_xs_1
+    if Rails.env.production?
+    else
+      'kassan-xs.jpg'
     end
   end
 end
