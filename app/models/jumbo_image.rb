eb44fba9576758571097d9612ba21d5852e40337
jgittler <jason.gittler@gmail.com>
2015-09-04 17:41:31 -0700

    cdn urls

--- app/models/jumbo_image.rb
+++ app/models/jumbo_image.rb
@@ -9,6 +9,7 @@ class JumboImage
 
   def self.vosh
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407751/fello_and_vosh.jpg"
     else
       'fello_and_vosh.jpg'
     end
@@ -16,6 +17,7 @@ class JumboImage
 
   def self.cate_lg
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407755/cate_lg.jpg"
     else
       'cate_lg.jpg'
     end
@@ -23,6 +25,7 @@ class JumboImage
 
   def self.share
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407827/konvoy_on_rock.jpg"
     else
       'konvoy_on_rock.jpg'
     end
@@ -30,6 +33,7 @@ class JumboImage
 
   def self.smile_girl
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407826/smile_girl.jpg"
     else
       'smile_girl.jpg'
     end
@@ -37,6 +41,7 @@ class JumboImage
 
   def self.thumbs_up
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407798/thumbs_up.jpg"
     else
       'thumbs_up.jpg'
     end
@@ -44,6 +49,7 @@ class JumboImage
 
   def self.smile_gown
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407826/smile_gown.jpg"
     else
       'smile_gown.jpg'
     end
@@ -51,6 +57,7 @@ class JumboImage
 
   def self.eye_exam
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407855/eye_exam.jpg"
     else
       'eye_exam.jpg'
     end
@@ -58,6 +65,7 @@ class JumboImage
 
   def self.eye_cover
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407855/eye_cover.jpg"
     else
       'eye_cover.jpg'
     end
@@ -65,6 +73,7 @@ class JumboImage
 
   def self.tl_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407800/matt.jpg"
     else
       'matt.jpg'
     end
@@ -72,6 +81,7 @@ class JumboImage
 
   def self.tm_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407786/owen.jpg"
     else
       'owen.jpg'
     end
@@ -79,6 +89,7 @@ class JumboImage
 
   def self.tr_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407785/kayrah.jpg"
     else
       'kayrah.jpg'
     end
@@ -86,6 +97,7 @@ class JumboImage
 
   def self.ml_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407786/ridge_down.jpg"
     else
       'ridge_down.jpg'
     end
@@ -93,6 +105,7 @@ class JumboImage
 
   def self.mm_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407854/fello_center.png"
     else
       'fello_center.png'
     end
@@ -100,6 +113,7 @@ class JumboImage
 
   def self.mr_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407755/dani.jpg"
     else
       'dani.jpg'
     end
@@ -107,6 +121,7 @@ class JumboImage
 
   def self.bl_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407755/cate.jpg"
     else
       'cate.jpg'
     end
@@ -114,6 +129,7 @@ class JumboImage
 
   def self.bm_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407801/kassan.jpg"
     else
       'kassan.jpg'
     end
@@ -121,6 +137,7 @@ class JumboImage
 
   def self.br_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407840/jerome_smile.jpg"
     else
       'jerome_smile.jpg'
     end
@@ -128,6 +145,7 @@ class JumboImage
 
   def self.tl_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407827/matt-xs.jpg"
     else
       'matt-xs.jpg'
     end
@@ -135,6 +153,7 @@ class JumboImage
 
   def self.tr_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407782/kayrah-xs.jpg"
     else
       'kayrah-xs.jpg'
     end
@@ -142,6 +161,7 @@ class JumboImage
 
   def self.ml_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407785/ridge_down-xs.jpg"
     else
       'ridge_down-xs.jpg'
     end
@@ -149,6 +169,7 @@ class JumboImage
 
   def self.mm_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407854/fello_center-xs.png"
     else
       'fello_center-xs.png'
     end
@@ -156,6 +177,7 @@ class JumboImage
 
   def self.mr_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407751/dani-xs.jpg"
     else
       'dani-xs.jpg'
     end
@@ -163,6 +185,7 @@ class JumboImage
 
   def self.bl_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407751/cate-xs.jpg"
     else
       'cate-xs.jpg'
     end
@@ -170,6 +193,7 @@ class JumboImage
 
   def self.bm_xs_1
     if Rails.env.production?
+      "http://res.cloudinary.com/hxeuqektg/image/upload/v1441407839/kassan-xs.jpg"
     else
       'kassan-xs.jpg'
     end
