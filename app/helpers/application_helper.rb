4afaa593d20cd905fab88774ba5fd1a95bacccef
jgittler <jason.gittler@gmail.com>
2016-01-05 22:05:24 -0800

    change content for g search, remove old methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,19 +5,6 @@ module ApplicationHelper
     end
   end
 
-  def charity_text
-    "Approximately 670 million people worldwide are functionally blind or visually impaired
-    because they do not have a pair of eyeglasses."
-  end
-
-  def vosh_video
-    link_to "VOSH Vision", "http://vosh.org/why-vision/", target: "_blank"
-  end
-
-  def vosh_donate
-    link_to "Donate Now", "http://vosh.org/donate", target: "_blank"
-  end
-
   def image_helper(name, options = {})
     if Rails.env.development?
       image_tag name, options
