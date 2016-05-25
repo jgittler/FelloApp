3fb7ed34daf64de6b4a7b226cdf7488c1fb1a7a1
jgittler <jason.gittler@gmail.com>
2016-05-25 00:18:37 -0700

    set up auth for mg webhook

--- app/controllers/mailgun_controller.rb
+++ app/controllers/mailgun_controller.rb
@@ -1,32 +1,54 @@
 require "openssl"
 
 class MailgunController < ApplicationController
-  # before_action :auth_webhook, only: :open_webhook
+  before_action :auth_webhook, only: :open_webhook
 
   def open_webhook
-    puts
-    puts
-    puts params
-    puts
-    puts
-    puts "IM HERE"
-    puts
-    puts
-
-    no_view
+    render json: { params: open_event_params }, status: 200
   end
 
   private
 
-  # def verify(api_key, token, timestamp, signature)
-  #   digest = OpenSSL::Digest::SHA256.new
-  #   data = [timestamp, token].join
-  #   signature == OpenSSL::HMAC.hexdigest(digest, api_key, data)
-  # end
-  #
-  # def auth_webhook
-  #   unless verify
-  #     render json: { errors: "You are not allowed to post here." }, status: :unauthorized
-  #   end
-  # end
+  def verified(api_key, token, timestamp, signature)
+    digest = OpenSSL::Digest::SHA256.new
+    data = [timestamp, token].join
+    signature == OpenSSL::HMAC.hexdigest(digest, api_key, data)
+  end
+
+  def auth_webhook
+    unless verified(mg_api_key, mg_token, event_time, mg_signature)
+      render json: { errors: "You are not allowed to post here." }, status: 406
+    end
+  end
+
+  def mg_api_key
+    Rails.application.secrets.mailgun_api_key
+  end
+
+  def mg_token
+    open_event_params.fetch("token")
+  end
+
+  def event_time
+    open_event_params.fetch("timestamp")
+  end
+
+  def mg_signature
+    open_event_params.fetch("signature")
+  end
+
+  def open_event_params
+    params.permit(
+      :city,
+      :country,
+      :region,
+      :recipient,
+      :timestamp,
+      :token,
+      :signature,
+      "device-type",
+      "client-name",
+      "client-os"
+    )
+  end
 end
